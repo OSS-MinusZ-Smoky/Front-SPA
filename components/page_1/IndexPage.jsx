1e65deabaf87550ab305f210374d1f284b5866b1
HamSungJun <wleo12345@naver.com>
2018-09-29 14:16:56 +0900

    Google Map Marking

--- components/page_1/IndexPage.jsx
+++ components/page_1/IndexPage.jsx
@@ -3,32 +3,25 @@ import SharedStyle from '../../public/Styles/SharedStyle.css';
 import Appbar from '../particles/Appbar.jsx';
 import Appbody from '../particles/Appbody.jsx';
 
-let myHeaders = new Headers();
-let fetchUrl = 'http://18.191.27.239:8888/country=';
-
-const myInit =
- { 
-    method: 'GET',        
-    headers: myHeaders,                        
-    cache: 'default'              
- };
-
 class IndexPage extends React.Component{
   constructor(){
     super();
     this.state = {
-      contryInput : "",
-      contryName : "Searching...",
+
+      cityInput : "",
+      cityName : "Searching...",
       deviceInstalled : "None",
       smokingSeverity : "None",
       goodState : "None",
       badState : "None",
       disconnectedState : "None",
-      defaultZoom : 2,
-      defaultCenter : {lat : 40.463667 , lng : -3.74922},
+      defaultZoom : 8,
+      defaultCenter : {lat : 37.541 , lng : 126.986},
       eventZoom : null,
       eventCenter : null,
-      
+      isResponsed : false,
+      markerPos : null,
+
     }
     this.handleContryInput = this.handleContryInput.bind(this);
   }
@@ -37,64 +30,82 @@ class IndexPage extends React.Component{
     let CONTRY_INPUT = document.querySelector('#CONTRY-INPUT');
     CONTRY_INPUT.addEventListener('keyup',this.handleContryInput);
     CONTRY_INPUT.addEventListener('touchend',this.handleContryInput);
-    
+     
   }
 
   handleContryInput(event){
     
     // fetch() -> Get respose -> lists info by state changing
-    if(event.keyCode == 13 && this.state.eventZoom){
-      
-      let HTTP_REQUEST = new XMLHttpRequest();
-      HTTP_REQUEST.open('GET','/go/?contry='+this.state.contryName,true);
-      HTTP_REQUEST.send(null);
-      window.location.href = './go/?contry='+this.state.contryName
-      
-    }
-
-    if(event.target.value.length >= 3 && event.keyCode != 13){
-      
-      fetch(fetchUrl+event.target.value,myInit).then((response)=>{
-        let Jres = response.json();
-        return Jres;
-        }).then((Jres) => {
-          const responsePos = {
-            lat : parseFloat(Jres[0].lat),
-            lng : parseFloat(Jres[0].long)
-          }
-          this.setState({
-            eventZoom : 6,
-            eventCenter : responsePos,
-            contryName : Jres[0].name,
-            deviceInstalled : Jres[0].num,
-            smokingSeverity : Jres[0].status,
-            defaultZoom : null,
-            defaultCenter : null,
-          })
-          console.log(this.state.pos)
-        });
-
-    }
-    else if(event.target.value.length == 0 && event.keyCode != 13){
-
+    if(event.keyCode != 13){
       this.setState({
-
-        defaultZoom : 2,
-        defaultCenter : {lat : 40.463667 , lng : -3.74922},
+        cityInput : event.target.value,
+        cityName : "Searching...",
+        deviceInstalled : "None",
+        smokingSeverity : "None",
+        goodState : "None",
+        badState : "None",
+        disconnectedState : "None",
         eventZoom : null,
         eventCenter : null,
-        contryName : "Searching...",
-        deviceInstalled : "None",
-        smokingSeverity : "None"
-
+        isResponsed : false,
+        markerPos : null,
       })
-
     }
+    else if(event.keyCode == 13){
+
+      fetch('http://18.191.27.239:9999?city='+this.state.cityInput).then((response)=>{
+      let Jres = response.json();
+      return Jres;
+    }).then((Jres)=>{
+      if(Jres.status == 200){
+
+        const resCityName = Jres.result2[0].city
+        const resInstalled = parseInt(Jres.result2[0].num)
+        const resGood = parseInt(Jres.result2[0].good_status)
+        const resBad = parseInt(Jres.result2[0].bad_status)
+        const resDiscon = parseInt(Jres.result2[0].disconnect)
+        const resLat = parseFloat(Jres.result2[0].lat)
+        const resLng = parseFloat(Jres.result2[0].long)
+
+        let Severity;
+        if((resBad / resInstalled <= 0.2)){
+          Severity = "Very Good" 
+        }
+        else if(0.2 < (resBad / resInstalled) <= 0.4){
+          Severity = "Good" 
+        }
+        else if(0.4 < (resBad / resInstalled) <= 0.6){
+          Severity = "Normal" 
+        }
+        else if(0.6 < (resBad / resInstalled) <= 0.8){
+          Severity = "Bad" 
+        }
+        else if(0.8 < (resBad / resInstalled) <= 1){
+          Severity = "Very Bad" 
+        }
+         
+        this.setState({
+          cityName : resCityName,
+          deviceInstalled : resInstalled,
+          smokingSeverity : Severity,
+          goodState : resGood,
+          badState : resBad,
+          disconnectedState : resDiscon,
+          defaultZoom : null,
+          defaultCenter : null,
+          eventZoom : 13,
+          eventCenter : {lat : resLat , lng : resLng},
+          isResponsed : false,
+          markerPos : Jres.result1
+        })
+      }
+    }) 
+    }
+    
   }
 
   render(){
 
-    
       return(
 
         <div className="Wrapper">
@@ -102,14 +113,16 @@ class IndexPage extends React.Component{
           <Appbody
           defaultCenter={this.state.defaultCenter}
           defaultZoom={this.state.defaultZoom}
-          eventZoom={this.state.eventZoom}
           eventCenter={this.state.eventCenter}
-          contryName={this.state.contryName}
+          eventZoom={this.state.eventZoom}
+          cityName={this.state.cityName}
           deviceInstalled={this.state.deviceInstalled}
           smokingSeverity={this.state.smokingSeverity}
           goodState={this.state.goodState}
           badState={this.state.badState}
           disconnectedState={this.state.disconnectedState}
+          isResponsed={this.state.isResponsed}
+          markerObj={this.state.markerPos}
           />
         </div>
         
